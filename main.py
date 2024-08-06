a06aa2653f7d9e710da75a5241b13ba6d94f9ea5
Jordan Khong <hello@jyodann.com>
2024-08-06 22:18:25 +0800

    Attempt to Parse Yaml

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from ruamel.yaml import YAML
+import pathlib
 
-import os
+# Supported Archs:
+# ['x64', 'x86', 'arm64', 'arm', 'neutral']
 
-for subdir, dirs, files in os.walk("./manifest/manifests"):
-    for file in files:
-        print(os.path.join(subdir, file))
+yaml=YAML(typ='safe') 
+archs = []
+for application in pathlib.Path("./manifest/manifests/").rglob("*.installer.yaml"):
+    print(application)
+    file = yaml.load(application)
+    pass
