a9be37a8363f7b0333ac4b856d049d345d5d5dc8
Jordan Khong <hello@jyodann.com>
2024-08-06 23:08:20 +0800

    Identify Architecture of application

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from ruamel.yaml import YAML
 import pathlib
 
+#can also use file command to determine architecture
+
 # Supported Archs:
 # ['x64', 'x86', 'arm64', 'arm', 'neutral']
 
 yaml=YAML(typ='safe') 
 archs = []
 for application in pathlib.Path("./manifest/manifests/").rglob("*.installer.yaml"):
-    print(application)
+    
     file = yaml.load(application)
+    print(file["PackageIdentifier"], file["PackageVersion"])
+    print(set([arch["Architecture"] for arch in file["Installers"]]))
     pass
