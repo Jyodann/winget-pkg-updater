e25b6eca3664ae82d7b2c05f80545767e8e3762a
Jordan Khong <hello@jyodann.com>
2024-08-07 18:23:45 +0800

    Export Database

--- main.py
+++ main.py
@@ -6,23 +6,27 @@ import os
 
 # Supported Archs:
 # ['x64', 'x86', 'arm64', 'arm', 'neutral']
-
+db_name = "winget-pkg-app.db"
 yaml=YAML(typ='safe') 
 archs = []
-i = 0
+application_no = 0
+num_app_and_archs = 0
+if (os.path.exists(db_name)):
+    os.remove(db_name)
 
-if (os.path.exists("test.db")):
-    os.remove("test.db")
-
-con = sqlite3.connect('test.db')
+con = sqlite3.connect(db_name)
 new_cur = con.cursor()
 
 new_cur.execute('''
 CREATE TABLE "Applications" (
 	"Id"	INTEGER NOT NULL UNIQUE,
-	"Name"	TEXT NOT NULL,
+    "Name"	TEXT NOT NULL,
+	"Identifier"	TEXT NOT NULL,
 	"Version"	TEXT NOT NULL,
-	"Achitecture"	TEXT NOT NULL,
+	"Publisher"	TEXT NOT NULL,
+    "Licence"	TEXT NOT NULL,
+    "ShortDescription"	TEXT NOT NULL,       
+    "Architecture"	TEXT NOT NULL,  
 	PRIMARY KEY("Id" AUTOINCREMENT)
 )
 ''')
@@ -30,17 +34,39 @@ CREATE TABLE "Applications" (
 con.commit()
 
 execute_command = []
-for application in pathlib.Path("./manifest/manifests/").rglob("*.installer.yaml"):
-    file = yaml.load(application)
-    indentifier = file["PackageIdentifier"]
-    version =  file["PackageVersion"]
+for application_installer_path in pathlib.Path("./manifest/manifests/").rglob("*.installer.yaml"):
+
+    # <PackageIdentifier>.installer.yaml
+    installer_file = yaml.load(application_installer_path)
+    
+    identifier = installer_file["PackageIdentifier"]
+    version =  installer_file["PackageVersion"]
+
+    # <PackageIdentifier>.yaml
+    manifest_version_path = application_installer_path.parent.joinpath(f"{identifier}.yaml")
+    version_info = yaml.load(manifest_version_path)
+    
+    default_locale = version_info["DefaultLocale"]
+
+    # <PackageIdentifier>.locale.<defaultLocale>.yaml
+    manifest_metadata_path = application_installer_path.parent.joinpath(f"{identifier}.locale.{default_locale}.yaml")
+    metadata = yaml.load(manifest_metadata_path)
+
+    publisher = metadata["Publisher"]
+    package_name = metadata["PackageName"]
+    license = metadata["License"]
+    short_desc = metadata["ShortDescription"]
     archs = set()
-    for installers in file["Installers"]:
-        archs.add( (indentifier, version, installers["Architecture"]) )
+    for installers in installer_file["Installers"]:
+        architecture = installers["Architecture"]
+        archs.add( (package_name, identifier, version, publisher, license, short_desc, architecture) )
     
     for data in archs:
         execute_command.append(data)
-        i += 1
-con.executemany("INSERT INTO Applications(Name, Version, Achitecture) VALUES(?, ?, ?)", execute_command)
+        num_app_and_archs += 1
+
+    application_no += 1
+
+con.executemany("INSERT INTO Applications(Name, Identifier, Version, Publisher, Licence, ShortDescription, Architecture) VALUES(?, ?, ?, ?, ?, ?, ?)", execute_command)
 con.commit()
-print(i)
+print(f"Added {application_no} Packages with {num_app_and_archs} Different Installers")
