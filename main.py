1c142f18d050b369ae4d4ffb1914074d87bf9484
Jordan <hello@jyodann.com>
2024-08-09 17:19:56 +0800

    Changed Manifest Path

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def main():
 
     con.commit()
 
-    app_paths = list(pathlib.Path("./winget-pkgs/winget-pkgs/manifests/").glob("*/"))
+    app_paths = list(pathlib.Path("./winget-pkgs/manifests/").glob("*/"))
 
     with Pool(len(app_paths)) as p:
         executed = p.map(get_app_data, app_paths)
