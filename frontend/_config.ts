1121cbc2c5dccff5d4a2bddea5d4b0f19c15d454
Jordan Khong <hello@jyodann.com>
2024-08-10 17:40:01 +0800

    Added Main Page

--- frontend/_config.ts
+++ frontend/_config.ts
@@ -7,7 +7,34 @@ import basePath from "lume/plugins/base_path.ts";
 const site = lume({
   location: new URL("https://jyodann.github.io/winget-pkg-updater/"),
 });
-site.use(tailwindcss());
+site.use(
+  tailwindcss({
+    options: {
+      theme: {
+        extend: {
+          fontFamily: {
+            outfit: ["Outfit", "sans-serif"],
+          },
+          colors: {
+            jacarta: {
+              "50": "#edf0ff",
+              "100": "#dfe2ff",
+              "200": "#c5caff",
+              "300": "#a2a7ff",
+              "400": "#7f7cfd",
+              "500": "#6c5ef6",
+              "600": "#5e40eb",
+              "700": "#5032d0",
+              "800": "#412ca7",
+              "900": "#2e236c",
+              "950": "#22194d",
+            },
+          },
+        },
+      },
+    },
+  })
+);
 site.use(postcss());
 site.use(
   pagefind({
