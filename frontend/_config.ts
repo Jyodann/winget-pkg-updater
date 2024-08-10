2d395c3fc525b69522732ecb118218fdf1d6ff80
Jordan <hello@jyodann.com>
2024-08-10 12:36:13 +0800

    Changed to Use Relative URLS, Temporarily Removed Cloudflare

--- frontend/_config.ts
+++ frontend/_config.ts
@@ -2,11 +2,20 @@ import lume from "lume/mod.ts";
 import tailwindcss from "lume/plugins/tailwindcss.ts";
 import postcss from "lume/plugins/postcss.ts";
 import pagefind from "lume/plugins/pagefind.ts";
-
+import relativeUrls from "lume/plugins/relative_urls.ts";
 const site = lume();
 
 site.use(tailwindcss(/* Options */));
 site.use(postcss());
-site.use(pagefind(/* Options */));
+site.use(
+  pagefind({
+    indexing: {
+      rootSelector: "html",
+      verbose: false,
+    },
+  })
+);
+
+site.use(relativeUrls());
 
 export default site;
